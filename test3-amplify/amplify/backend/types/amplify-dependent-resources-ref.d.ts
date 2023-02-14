4f55e358e33de83add9689afa9de2cc0aba4f159
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-14 10:34:46 -0600

    created react app

---
+++ test3-amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "test3amplifyc2f603ac": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
